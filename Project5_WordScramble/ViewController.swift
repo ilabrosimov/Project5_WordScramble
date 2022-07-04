e4a3aa72c68bfba03c70accce98725745649683d
Ilia <ilabrosimov@gmail.com>
2022-07-04 14:48:48 +0300

    Initial Commit

---
+++ Project5_WordScramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5_WordScramble
+//
+//  Created by Ilia Abrosimov on 04.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
